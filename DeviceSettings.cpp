208230cc56af0931da073ee30af1174180937788
Michael <mguntli@users.noreply.github.com>
2017-11-01 21:20:07 +0100

    Implemented WiFi settings
    Using offsetof to calculate offsets within struct.

--- DeviceSettings.cpp
+++ DeviceSettings.cpp
@@ -1,16 +1,41 @@
 #include "DeviceSettings.h"
 
+#include <Arduino.h>
+#include <assert.h>
 #include <EEPROM.h>
+#include <stddef.h>
 
-DeviceSettings::DeviceSettings() {
+/**
+ * ESP8622 EEPROM offers up to 4096 bytes (one flash block/sector).
+ * When you call EEPROM.begin(), that block is read and put into memory. 
+ * From then on unless you call commit() or end(), you are always working with that memory block and not the actual content of the flash block.
+ * When you call end() or commit(), the memory block is checked for changes and if it has any, the flash block is erased and the memory block is dumped to the flash to save the data for next time.
+ * Flash memory has a particular number of erase/write cycles on its bits (around 10'000), so commit() calls need to be optimized to prevent wearing the flash too soon.
+ */
+
+DeviceSettings::DeviceSettings(void) {    
+}
+
+void DeviceSettings::load(void) {
+    assert(CURRENT_EEPROM_LAYOUT_SIZE_BYTES <= 4096);
+    Serial.print("DeviceSettings: Loading EEPROM layout with size = "); Serial.println(CURRENT_EEPROM_LAYOUT_SIZE_BYTES);
+
+    // ESP8622 needs EEPROM.begin(), so it copies the flash block into a memory area
     EEPROM.begin(CURRENT_EEPROM_LAYOUT_SIZE_BYTES);
-    loadEeprom();
 }
 
-void DeviceSettings::loadEeprom() {
-    loadEepromLayoutV1();
+void DeviceSettings::save(void) {
+    EEPROM.commit();
 }
 
-void DeviceSettings::loadEepromLayoutV1() {
+DeviceSettings::WiFiSettings DeviceSettings::getWiFiSettings(void) {
+    WiFiSettings eepromValues;
+    size_t offset = offsetof(struct EepromLayoutV1, wifiSettings);
+    EEPROM.get(offset, eepromValues);
+    return eepromValues;
+}
 
+void DeviceSettings::setWiFiSettings(const WiFiSettings& settings) {
+    size_t offset = offsetof(struct EepromLayoutV1, wifiSettings);
+    EEPROM.put(offset, settings);
 }
--- DeviceSettings.h
+++ DeviceSettings.h
@@ -10,17 +10,94 @@
 
 class DeviceSettings {
 public:
+
+    struct DeviceParameters {
+        uint32_t deviceSerialNumber;
+    };
+
+    struct DeviceConfig {
+        union ConfigFlags {
+            struct {
+                bool maxVolumeLimitActive : 1;
+                bool stayAlwaysOnActive : 1;
+                // 30 flags remaining for future use
+            };
+            uint32_t flags;
+        };
+        char deviceName[32+1] = "";
+        // upper threshold the volume can be set (e.g. as a child lock)
+        uint8_t maxVolume;
+        // timeout in minutes when device should go to deep sleep after last user interaction
+        uint16_t deepSleepTimeout;
+    };
+    
+    struct WiFiSettings {
+        union ConfigFlags {
+            struct {
+                bool dhcpActive : 1;
+                // 32 flags remaining for future use
+            };
+            uint32_t flags;
+        };
+        // max SSID length is 32 characters, reserve 1 additional byte for string null termination
+        char ssid[32+1] = "";
+        // max passphrase length for WPA-PSK is 63 characters
+        char passphrases[63+1] = "";
+
+        struct IpConfig {
+            uint32_t address;
+            uint32_t subnetMask;
+            uint32_t dnsServer;
+            uint32_t gateway;
+        };
+        IpConfig fixedIpConfig;
+    };
+
+    struct PresetConfig {
+        struct Action {
+            enum ActionType {
+                PLAY_URI = 1,
+                VOLUME = 2,
+                GROUP_ALL_PLAYERS = 3,
+                TURN_OFF_TIMER = 4
+            };
+            uint8_t actionIdentifier;
+            // TODO: better way to be flexible for the future?
+            uint8_t actionParameters[32];    
+        };
+        
+        char mediaUri[255+1] = "";
+        uint8_t numberOfActionsActive;
+        Action actions[4];
+    };
+
+    /**
+     * Load settings from persistent storage and keep data in memory.
+     */
+    static void load(void);
+
+    /**
+     * Save settings from memory to persistent storage.
+     */
+    static void save(void);
+
+    /**
+     * Get the stored WiFi settings.
+     */
+    static WiFiSettings getWiFiSettings(void);
+
+    /**
+     * Set the WiFi settings.
+     * Note: save() has to be called in order to persist the data
+     */
+    static void setWiFiSettings(const WiFiSettings& settings);
+
+private:
     /**
     * Default constructor.
     */
-    DeviceSettings(void);
+    DeviceSettings(void);    
 
-    /**
-     * Erase persistent storage and restore factory defaults.
-     */
-    void resetFactoryDefaults(void);
-
-private:
     /**
      * Private copy constructor.
      */
@@ -31,92 +108,27 @@ private:
      */
     DeviceSettings& operator=(const DeviceSettings&);
 
-    void loadEeprom(void);
-    void storeEeprom(void);
-
-    void loadEepromLayoutV1(void);
-
-    void loadWiFiCredentials(void);
-    void storeWiFiCredentials(void);
-
-    // maximum EEPROM size is limited to 4096 bytes!
+    /**
+     * ESP8266 needs 4 byte alignment since it emulates EEPROM in flash.
+     * Note: sizeof() and offsetof(type, member) will calculate offsets taking alignment rules into account.
+     */
     struct EepromLayoutV1 {
+        // maximum EEPROM size is limited to 4096 bytes!
         uint8_t layoutVersion;
-        
-        // cannot be configured by the user and is configured in production
-        struct DeviceParameters {
-            uint32_t deviceSerialNumber;
-        };
-
-        struct DeviceConfig {
-            union ConfigFlags {
-                struct {
-                    bool maxVolumeLimitActive : 1;
-                    bool stayAlwaysOnActive : 1;
-                    // 30 flags remaining for future use
-                };
-                uint32_t flags;
-            };
-            char deviceName[32+1] = "";
-            // upper threshold the volume can be set (e.g. as a child lock)
-            uint8_t maxVolume;
-            // timeout in minutes when device should go to deep sleep after last user interaction
-            uint16_t deepSleepTimeout;
-        };
-        
-        struct WiFiSettings {
-            union ConfigFlags {
-                struct {
-                    bool dhcpActive : 1;
-                    // 32 flags remaining for future use
-                };
-                uint32_t flags;
-            };
-            // max SSID length is 32 characters, reserve 1 additional byte for string null termination
-            char ssid[32+1] = "";
-            // max passphrase length for WPA-PSK is 63 characters
-            char passphrases[63+1] = "";
-
-            struct IpConfig {
-                uint32_t address;
-                uint32_t subnetMask;
-                uint32_t dnsServer;
-                uint32_t gateway;
-            };
-            IpConfig fixedIpConfig;
-        };
-
-        // Sonos device to control
+        DeviceParameters deviceParameters;
+        WiFiSettings wifiSettings;
         char sonosDeviceName[32+1] = "";
-
-        struct PresetConfig {
-            struct Action {
-                enum ActionType {
-                    PLAY_URI = 1,
-                    VOLUME = 2,
-                    GROUP_ALL_PLAYERS = 3,
-                    TURN_OFF_TIMER = 4
-                };
-                uint8_t actionIdentifier;
-                // TODO: better way to be flexible for the future?
-                uint8_t actionParameters[32];    
-            };
-            
-            char mediaUri[255+1] = "";
-            uint8_t numberOfActionsActive;
-            Action actions[4];
-        };
         PresetConfig preset1;
         PresetConfig preset2;
         PresetConfig preset3;
         PresetConfig preset4;
-    };
+    }; __attribute__ ((aligned (4)));
 
     // constants
     // in order to extend the layout in future, we need to have a versioned layout of the EEPROM structure
     static const uint16_t EEPROM_LAYOUT_VERSION_V1 = 1;
     static const uint16_t CURRENT_EEPROM_LAYOUT_VERSION = EEPROM_LAYOUT_VERSION_V1;
-    static const uint16_t CURRENT_EEPROM_LAYOUT_SIZE_BYTES = 2048;
+    static const uint16_t CURRENT_EEPROM_LAYOUT_SIZE_BYTES = sizeof(EepromLayoutV1);
 
     // instance members
 };
