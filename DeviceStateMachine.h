c7124e99771dcd8ef4e804b5e227a81233a4c92f
Michael <mguntli@users.noreply.github.com>
2017-11-06 20:39:14 +0100

    Prototype for device state machine
    The state machine is the coordinator of the device

---
+++ DeviceStateMachine.h
@@ -0,0 +1,49 @@
+#ifndef DEVICESTATEMACHINE_H
+#define DEVICESTATEMACHINE_H
+
+#include <stdint.h>
+
+/**
+ * Command line interface for development to interact with the device.
+ */
+class DeviceStateMachine {
+public:
+
+    /**
+    * Default constructor.
+    */
+    DeviceStateMachine(void);
+
+    /**
+    * Destructor.
+    */
+    ~DeviceStateMachine(void);
+
+    // Touch input event handlers
+    void onEventVolumeUp(uint16_t volumeStepCount);
+    void onEventVolumeDown(uint16_t volumeStepCount);
+    void onEventPlayPause(void);
+    void onEventNext(void);
+    void onEventPrevious(void);
+    void onEventPreset1(void);
+    void onEventPreset2(void);
+    void onEventPreset3(void);
+    void onEventPreset4(void);
+    void onEventConfigMode(void);
+    void onEventRestart(void);
+
+private:
+
+    /**
+    * Private copy constructor.
+    */
+    DeviceStateMachine(const DeviceStateMachine&);
+   
+    /**
+    * Private assignment constructor.
+    */
+    DeviceStateMachine& operator=(const DeviceStateMachine&);
+
+};
+
+#endif //DEVICESTATEMACHINE_H
