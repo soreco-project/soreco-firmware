b2ce66a86af378e1a2b5005ba3fc5d668a7285e6
Sandro Pedrett <sandro.pedrett@hotmail.com>
2017-11-11 11:29:49 +0100

    Add empty device state machine cpp

---
+++ src/DeviceState/DeviceStateMachine.cpp
@@ -0,0 +1,54 @@
+#include "DeviceStateMachine.h"
+
+#include <Arduino.h>
+
+DeviceStateMachine::DeviceStateMachine(void) {
+
+}
+
+DeviceStateMachine::~DeviceStateMachine(void) {
+	
+}
+
+void DeviceStateMachine::onEventVolumeUp(uint16_t volumeStepCount) {
+}
+
+void DeviceStateMachine::onEventVolumeDown(uint16_t volumeStepCount) {
+
+}
+
+void DeviceStateMachine::onEventPlayPause(void) {
+	
+}
+
+void DeviceStateMachine::onEventNext(void) {
+	
+}
+
+void DeviceStateMachine::onEventPrevious(void) {
+	
+}
+
+void DeviceStateMachine::onEventPreset1(void) {
+	
+}
+
+void DeviceStateMachine::onEventPreset2(void) {
+	
+}
+
+void DeviceStateMachine::onEventPreset3(void) {
+	
+}
+
+void DeviceStateMachine::onEventPreset4(void) {
+	
+}
+
+void DeviceStateMachine::onEventConfigMode(void) {
+	
+}
+	
+void DeviceStateMachine::onEventRestart(void) {
+	
+}
