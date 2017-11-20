88f507853489e5b886741afae6d5e01fab45441a
Michael <mguntli@users.noreply.github.com>
2017-11-20 17:45:59 +0100

    Fixed Cppcheck issue about value never used
    Variable 'statusDecode' is assigned a value that is never used.

--- src/Wifi/Remote/RemoteCommunication.cpp
+++ src/Wifi/Remote/RemoteCommunication.cpp
@@ -56,7 +56,7 @@ bool RemoteCommunication::loop(void) {
 _Protocol RemoteCommunication::processRequest(byte* pData) {
     _Protocol builder = Protocol_init_zero;
     pb_istream_t streamIn = pb_istream_from_buffer(pData, WifiConfigMessage_size);
-    bool statusDecode = pb_decode(&streamIn, Protocol_fields, &builder);
+    pb_decode(&streamIn, Protocol_fields, &builder);
 
     return builder;
 }
